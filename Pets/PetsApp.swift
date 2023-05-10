dea686fe2a5d026391d7be579ee49b11e96820bf
Bilal Durnagöl <bilaldurnagol@gmail.com>
2023-05-11 00:07:34 +0300

    Initial Commit

---
+++ Pets/PetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsApp.swift
+//  Pets
+//
+//  Created by Bilal Durnagöl on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
